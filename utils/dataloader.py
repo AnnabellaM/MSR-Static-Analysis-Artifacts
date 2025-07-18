91fac37df11303c28d04fdccb5517a36d6386c92
Eric Zhang <ericspring08@gmail.com>
2025-07-18 11:20:52 -0500

    update dataloader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pathlib import Path
+import ast
 
 # datafolder path is ../data relative to the current file make sure it is always regardless of where the file is called
 data_folder_path = Path(__file__).resolve().parents[1] / 'data'
@@ -11,6 +12,8 @@ def get_repos():
 def get_issues(filter=None):
     if filter == None:
         issues_metadata = pd.read_csv(f'{data_folder_path}/issues_metadata.csv')
+        issues_metadata['timeline'] = issues_metadata['timeline'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+        issues_metadata['labels'] = issues_metadata['labels'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
         
         # check if exists
         if (data_folder_path / 'issues_properties.csv').exists():
@@ -22,6 +25,8 @@ def get_issues(filter=None):
             issues = issues_metadata
     elif filter == 'metadata':
         issues = pd.read_csv(f'{data_folder_path}/issues_metadata.csv')
+        issues['timeline'] = issues['timeline'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+        issues['labels'] = issues['labels'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
     elif filter == 'properties':
         issues = pd.read_csv(f'{data_folder_path}/issues_properties.csv')
     else:
